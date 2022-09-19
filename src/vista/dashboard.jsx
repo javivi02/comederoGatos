e4eebd1620eb1512b75a920145f78f454e6fa92f
Javier <javivi02@gmail.com>
2022-09-19 21:01:53 +0200

    retocando bordes

--- src/vista/dashboard.jsx
+++ src/vista/dashboard.jsx
@@ -10,7 +10,8 @@ const Dashboard = ({ props }) => {
         <>
             <div className={`flex flex-col items-center justify-center`}>
 
-                <main className={`h-full w-full sm:rounded-xl sm:w-[39rem] sm:mt-10 sm:border-4`}>
+                <main className={`h-full w-full sm:rounded-xl sm:w-[39rem] sm:mt-10 sm:border-4 sm:h-[980px]
+                sm:h-[640px]`}>
 
                     <NavBar />
                     <Insert />
